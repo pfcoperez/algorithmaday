3f3d05bdf62ed788ee02e36f36e82c4db81985d3
Pablo Fco. Pérez Hidalgo <contact@pablofranciscoperez.info>
2016-10-14 21:19:11 +0200

    More matrix operations and first version of recursive D&Q naive multiplication.

--- src/main/scala/org/pfcoperez/dailyalgorithm/Algebra.scala
+++ src/main/scala/org/pfcoperez/dailyalgorithm/Algebra.scala
@@ -1,6 +1,5 @@
 package org.pfcoperez.dailyalgorithm
 
-import org.pfcoperez.dailyalgorithm.Algebra.Matrix.NumericMatrix.MultiplicationMethod
 
 import scala.reflect.ClassTag
 
@@ -12,17 +11,17 @@ object Algebra {
 
     object NumericMatrix {
 
-      abstract class MultiplicationMethod {
-        def multiply[T : Numeric : ClassTag](A: Matrix[T], B: Matrix[T]): Matrix[T]
+      trait MultiplicationMethod {
+        def multiply[T: Numeric : ClassTag](A: Matrix[T], B: Matrix[T]): Matrix[T]
       }
 
-      object Implicits {
+      object MultiplicationMethods {
 
-        implicit val NaiveMultiplicationMethod = new MultiplicationMethod {
+        object NaiveMultiplicationMethod extends MultiplicationMethod {
 
           /**
             * O(n^3)
-            */
+            **/
           override def multiply[T: Numeric : ClassTag](A: Matrix[T], B: Matrix[T]): Matrix[T] = {
             val numericEvidence: Numeric[T] = implicitly[Numeric[T]]
             (0 until A.length) map (i =>
@@ -36,43 +35,107 @@ object Algebra {
 
         }
 
+        case class DivideAndConquer(fallback: MultiplicationMethod) extends MultiplicationMethod {
+
+          protected def mergeResults[T: ClassTag](parts: Matrix[Matrix[T]]): Matrix[T] = {
+            val (unitN, unitM) = size(parts.head.head)
+            positionalValues(unitN * parts.length, unitM * parts.head.length) {
+              (i, j) => parts(i / unitN)(j / unitM)(i % unitN)(j % unitM)
+            }
+          }
+
+          protected def splitInput[T: ClassTag](X: Matrix[T]): Matrix[Matrix[T]] = {
+            val (n, m) = size(X)
+            positionalValues(2, 2) {
+              (ui, uj) => positionalValues(n / 2, m / 2) {
+                (i, j) => X(ui * n / 2 + i)(uj * n / 2 + j)
+              }
+            }
+          }
+
+          /**
+            *
+            */
+          override def multiply[T: Numeric : ClassTag](A: Matrix[T], B: Matrix[T]): Matrix[T] = {
+            val (n, m) = (size(A)._1, size(B)._2)
+            if (n % 2 == 0 && m % 2 == 0) {
+              val List(splittedA, splittedB) = List(A,B).map(splitInput)
+              mergeResults {
+                positionalValues(2, 2) {
+                  (ui, uj) =>
+                    (zeros(n / 2, m / 2) /: (0 until 2))((sum,k) => sum + multiply(splittedA(ui)(k),splittedB(k)(uj)))
+                }
+              }
+            } else fallback.multiply(A, B)
+          }
+
+        }
+
       }
 
+      object Implicits {
+        implicit val DefaultMultiplicationMethod = MultiplicationMethods.NaiveMultiplicationMethod
+      }
+
+
     }
 
-    implicit class NumericMatrix[T : Numeric : ClassTag](m: Matrix[T]) {
+    implicit class NumericMatrix[T: Numeric : ClassTag](m: Matrix[T]) {
+
+      import org.pfcoperez.dailyalgorithm.Algebra.Matrix.NumericMatrix.MultiplicationMethod
+
       def *(that: Matrix[T])(implicit multiplicationMethod: MultiplicationMethod): Matrix[T] =
         multiplicationMethod.multiply(m, that)
+
+      def +(that: Matrix[T]): Matrix[T] = {
+        val (nrows, ncols) = size(that)
+        positionalValues(nrows, ncols)((i, j) => implicitly[Numeric[T]].plus(m(i)(j), that(i)(j)))
+      }
     }
 
-    def zeros[T : Numeric : ClassTag](n: Int, m: Int): Matrix[T] =
+    def zeros[T: Numeric : ClassTag](n: Int, m: Int): Matrix[T] =
       Array.fill(n)(Array.fill(m)(implicitly[Numeric[T]].zero))
 
-    def positionalValues[T : ClassTag](n: Int, m: Int)(pos2value: (Int, Int) => T): Matrix[T] =
-      (0 until n) map (i => (0 until m) map (pos2value(i,_)) toArray) toArray
+    def positionalValues[T: ClassTag](n: Int, m: Int)(pos2value: (Int, Int) => T): Matrix[T] =
+      (0 until n) map (i => (0 until m) map (pos2value(i, _)) toArray) toArray
 
-    def identity[T : Numeric : ClassTag](n: Int, m: Int): Matrix[T] =
+    def identity[T: Numeric : ClassTag](n: Int, m: Int): Matrix[T] =
       positionalValues(n, m) {
         case (i, j) if i == j => implicitly[Numeric[T]].one
         case _ => implicitly[Numeric[T]].zero
       }
 
+    def fmap[T, S: ClassTag](M: Matrix[T], f: T => S): Matrix[S] = {
+      val (n, m) = size(M)
+      positionalValues(n, m)((i, j) => f(M(i)(j)))
+    }
+
+    def size[T](m: Matrix[T]): (Int, Int) = m.length -> m.head.length
+
   }
+
 
   /* Use example
   import Matrix._
-  import Matrix.NumericMatrix.Implicits.NaiveMultiplicationMethod
+  //import Matrix.NumericMatrix.Implicits.DefaultMultiplicationMethod
+  import org.pfcoperez.dailyalgorithm.Algebra.Matrix.NumericMatrix.MultiplicationMethod
+  import org.pfcoperez.dailyalgorithm.Algebra.Matrix.NumericMatrix.MultiplicationMethods.DivideAndConquer
 
-  val a: Matrix[Int] = Array(Array(1,2,3), Array(4,5,6), Array(7,8,9))
-  val b: Matrix[Int] = identity(3,3)
+  implicit val _: MultiplicationMethod = DivideAndConquer(Matrix.NumericMatrix.MultiplicationMethods.NaiveMultiplicationMethod)
 
-  val res = a*b
+  val a: Matrix[Int] = Array(Array(1, 2, 3, 4), Array(5, 6, 7, 8), Array(9, 10, 11, 12), Array(13, 14, 15, 16))
+  val b: Matrix[Int] = identity(4, 4)
 
-  for(row <- res) {
-    for(cell <- row) print(s"$cell ")
-    println
-  }
-  */
+  val res = a * b
+
+  printMatrix(res)
+
+  def printMatrix[T](m: Matrix[T]): Unit = {
+    for (row <- m) {
+      for (cell <- row) print(s"$cell ")
+      println
+    }
+  }*/
 
 
 }
