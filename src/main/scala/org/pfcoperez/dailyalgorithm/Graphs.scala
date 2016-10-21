738f850961cac343878e984998536d546389d3e3
Pablo Fco. Pérez Hidalgo <contact@pablofranciscoperez.info>
2016-10-21 23:45:13 +0200

    Depth first search algorithm: O(n)

--- src/main/scala/org/pfcoperez/dailyalgorithm/Graphs.scala
+++ src/main/scala/org/pfcoperez/dailyalgorithm/Graphs.scala
@@ -8,6 +8,9 @@ object Graphs {
   case class Node[T](value: T, children: List[Node[T]]) extends DirectedGraph[T]
   case class Empty[T]() extends DirectedGraph[T]
 
+  /**
+    * O(n), n = number of nodes
+    */
   def breadthFirstTake[T, S](g: DirectedGraph[T])(f: PartialFunction[T, S]): List[S] = {
 
     def traverseRec(toVisit: Queue[DirectedGraph[T]], acc: List[S]): List[S] = toVisit.headOption collect {
@@ -19,6 +22,33 @@ object Graphs {
     } getOrElse acc.reverse
 
     traverseRec(Queue(g), Nil)
+
+  }
+
+  /**
+    * O(n), n = number of nodes
+    *
+    * Use example:
+    *
+    * val g: DirectedGraph[Int] =
+    *   Node(1, Node(2, Node(5, Nil)::Nil):: Node(3, Node(6, Node(7, Nil)::Nil)::Nil)::Node(4, Nil)::Nil)
+    *
+    * depthFirstTake(g) { case x => x }
+    *
+    */
+  def depthFirstTake[T, S](g: DirectedGraph[T])(f: PartialFunction[T, S]): List[S] = {
+
+    type Stack[T] = List[T]
+
+    def traverseRec(toVisit: Stack[DirectedGraph[T]], acc: List[S]): List[S] = toVisit.headOption collect {
+      case Node(value, children) =>
+        val (newToVisit, toAcc) = f.lift(value) map { res =>
+          (children ++ toVisit.tail, Some(res))
+        } getOrElse(toVisit.tail -> None)
+        traverseRec(newToVisit, toAcc map (_::acc) getOrElse acc)
+    } getOrElse acc.reverse
+
+    traverseRec(g::Nil, Nil)
 
   }
 
