5d2a744b644520c8a06679335c7ba00486a20e31
pfcoperez <contact@pablofranciscoperez.info>
2016-11-28 18:27:30 +0100

    A queue with two stacks

---
+++ src/main/scala/org/pfcoperez/dailyalgorithm/StacksAndQueues.scala
@@ -0,0 +1,35 @@
+package org.pfcoperez.dailyalgorithm
+
+object StacksAndQueues {
+
+  object CustomQueue {
+    def empty[T]: CustomQueue[T] = new CustomQueue[T](Nil, Nil)
+  }
+
+  class CustomQueue[T] private (
+                                 private val heads: List[T],
+                                 private val tails: List[T]) {
+
+    /* O(1) */
+    def head: Option[T] = heads.headOption
+
+    /* Average: O(1) */
+    def enqueue(x: T): CustomQueue[T] =
+      keepInvariants(new CustomQueue[T](heads, x::tails))
+
+    def dequeue: CustomQueue[T] =
+      keepInvariants(
+        new CustomQueue(heads.headOption.toList.flatMap(_ => heads.tail), tails)
+      )
+
+    /* Average: O(1), Worst: O(n) */
+    private def keepInvariants(queue: CustomQueue[T]): CustomQueue[T] =
+      (queue.heads, queue.tails) match {
+        case (Nil, qtails) =>
+          new CustomQueue[T](qtails reverse, Nil)
+        case _ => queue
+      }
+
+  }
+
+}
