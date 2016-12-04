6177897d839b708c47e7e416ebf5a1e3575abc24
pfcoperez <contact@pablofranciscoperez.info>
2016-12-04 13:00:44 +0100

    Heap: As functional data structure

--- src/main/scala/org/pfcoperez/dailyalgorithm/StacksAndQueues.scala
+++ src/main/scala/org/pfcoperez/dailyalgorithm/StacksAndQueues.scala
@@ -32,4 +32,42 @@ object StacksAndQueues {
 
   }
 
+  object Heap {
+
+    type Heap[T] = Vector[T]
+
+    def enqueue[T : Ordering](heap: Heap[T])(x: T): Heap[T] =
+      bubbleUp(heap :+ x, heap.size)
+
+    def dequeue[T : Ordering](heap: Heap[T]): Heap[T] =
+      bubbleDown(heap.updated(0, heap.last).init, 0)
+
+    private def bubbleUp[T](queue: Heap[T], idx: Int)(implicit ordering: Ordering[T]): Heap[T] = {
+      import ordering.mkOrderingOps
+
+      val parentIdx: Int = idx/2 + (if(idx%2 == 0) -1 else 0)
+
+      if(idx == 0 || queue(idx)>=queue(parentIdx))
+        queue
+      else
+        bubbleUp(queue.updated(parentIdx, queue(idx)).updated(idx, queue(parentIdx)), parentIdx)
+    }
+
+    private def bubbleDown[T](queue: Heap[T], idx: Int)(implicit ordering: Ordering[T]): Heap[T] =
+      if(idx >= queue.size-1) queue
+      else {
+        import ordering.mkOrderingOps
+
+        val leftIdx = idx*2+1
+        val rightIdx = idx*2+2
+
+        val swapWith = Seq(leftIdx, rightIdx).filter(_ < queue.size).minBy(queue)
+
+        if(queue(idx) < queue(swapWith)) queue
+        else bubbleDown(queue.updated(swapWith, queue(idx)).updated(idx, queue(swapWith)), swapWith)
+
+      }
+
+  }
+
 }
