e9a71804acff3a23ce7b013040c190696a6f8c86
Pablo Fco. Pérez Hidalgo <pfperez@stratio.com>
2017-01-12 17:52:50 +0100

    Binary tree projection into 2D matrix.

---
+++ src/main/scala/org/pfcoperez/dailyalgorithm/applications/ProjectedBinaryTree.scala
@@ -0,0 +1,71 @@
+package org.pfcoperez.dailyalgorithm.applications
+
+import org.pfcoperez.dailyalgorithm.Graphs.BinaryTrees._
+
+object ProjectedBinaryTree extends App {
+
+  /**
+    * Builds an sparse 2D matrix of the projection of a binary tree into
+    * a 2D plane using an uniform distance between a node and its children.
+    *
+    * O(n), n = Number of nodes.
+    *
+    */
+  def projectBinaryTree[T](btree: BinaryTree[T]): Map[(Int, Int), Seq[T]] = {
+
+    def projection(
+                    currentNode: BinaryTree[T],
+                    currentPosition: (Int, Int),
+                    acc: Map[(Int, Int), Seq[T]] = Map.empty
+                  ): Map[(Int, Int), Seq[T]] = currentNode match {
+
+      case Empty => acc
+      case Node(left, v, right) =>
+        val (i, j) = currentPosition
+
+        val fromLeft = projection(left, (i+1, j-1), acc)
+
+        val withCurrent = fromLeft + {
+          currentPosition -> (v +: (acc get currentPosition getOrElse Seq()))
+        }
+
+        projection(right, (i+1, j+1), withCurrent) // From right sub-tree
+
+    }
+
+    val projectionWidth = RawBinaryTree.height(btree)+1
+
+    projection(btree, (0, projectionWidth/2)).mapValues(_.reverse)
+
+
+  }
+
+  // A sample
+
+  val example = ((Empty: BinaryTree[Int]) /: (1 to 20)) {
+    (tree, element) => BalancedBinaryTree.insert(tree)(element)
+  }
+
+  val exampleProjection = projectBinaryTree(example)
+
+  val (maxi, maxj) = exampleProjection.keys.max
+
+  def boxedString[T](x: T, l: Int = 0): String =
+    x.toString.reverse.zipAll(1 to l, ' ', 0).reverse.map(_._1).mkString
+
+  val l = 5
+  for(i <- 0 to maxi) {
+    for(j <- 0 to maxj ) {
+      print(
+        boxedString(
+          exampleProjection get(i -> j) map ( v =>
+            boxedString(v.mkString(","), l)
+          ) getOrElse " "*l
+        )
+      )
+    }
+    println
+  }
+
+
+}
