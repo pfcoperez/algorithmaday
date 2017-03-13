cbb27e6ce22a385c026b53412062686391224763
pfcoperez <contact@pablofranciscoperez.info>
2017-03-13 20:02:20 +0100

    Maps as databases: O(n)

---
+++ src/main/scala/org/pfcoperez/dailyalgorithm/Databases.scala
@@ -0,0 +1,30 @@
+package org.pfcoperez.dailyalgorithm
+
+object Databases {
+
+  implicit class JoinOperations[K, V](thisMap: Map[K, V]) {
+
+    /**
+      * Inner join like operation
+      * O(n), n = left map size
+      */
+    def joinInner[VT](that: Map[K, VT]): Map[K, (V,VT)] =
+      for((k,va) <- thisMap; vb <- that.get(k)) yield k -> (va, vb)
+
+    /**
+      * Outer left join like operation
+      * O(n), n = left map size
+      */
+    def joinOuterLeft[VT](that: Map[K, VT]): Map[K, (V,Option[VT])] =
+      for((k,va) <- thisMap) yield k -> (va, that.get(k))
+
+    /**
+      * Right outer join like operation
+      * O(n), n = right map size
+      */
+    def joinOuterRight[VT](that: Map[K, VT]): Map[K, (Option[V],VT)] =
+      for((k,vb) <- that) yield k -> (thisMap.get(k), vb)
+
+  }
+
+}
---
+++ src/test/scala/org/pfcoperez/dailyalgorithm/database/JoinOperationsSpec.scala
@@ -0,0 +1,42 @@
+package org.pfcoperez.dailyalgorithm.database
+
+import org.scalatest.{FlatSpec, Matchers}
+import org.pfcoperez.dailyalgorithm.Databases._
+
+class JoinOperationsSpec extends FlatSpec with Matchers {
+
+  val ma = Map(0 -> " ", 1 -> "a", 2 -> "b")
+  val mb = Map(1 -> "A", 2 -> "B", 3 -> "C")
+
+  "An map with extended db operations" should "be able to perform inner joins" in {
+
+    (ma joinInner mb) shouldBe Map(1 -> ("a", "A"), 2 -> ("b", "B"))
+
+  }
+
+  it should "be able to perform left outer joins" in {
+
+    val expected = Map(
+      0 -> (" ", None     ),
+      1 -> ("a", Some("A")),
+      2 -> ("b", Some("B"))
+    )
+
+    (ma joinOuterLeft mb) shouldBe expected
+
+  }
+
+  it should "be able to perform right outer joins" in {
+
+    val expected = Map(
+      1 -> (Some("a"), "A"),
+      2 -> (Some("b"), "B"),
+      3 -> (None     , "C")
+    )
+
+    (ma joinOuterRight mb) shouldBe expected
+
+  }
+
+
+}
