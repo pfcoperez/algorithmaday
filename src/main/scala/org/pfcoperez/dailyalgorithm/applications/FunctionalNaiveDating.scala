12c3381d9f9d946273a60986fde91ea7b6544fff
pfcoperez <contact@pablofranciscoperez.info>
2017-03-18 22:11:17 +0100

    Simple matching/recomender algorithm: O(N*M)

---
+++ src/main/scala/org/pfcoperez/dailyalgorithm/applications/FunctionalNaiveDating.scala
@@ -0,0 +1,65 @@
+package org.pfcoperez.dailyalgorithm.applications
+
+import cats._
+import cats.implicits._
+
+object FunctionalNaiveDating extends App {
+
+  object CandidateSelectionSystem {
+
+    val maleCandidates: List[String] = List(
+      "LeChuck",
+      "Victor"
+    )
+
+    val femaleCandidates: List[String] = List(
+      "Elaine",
+      "Carla",
+      "Victoria"
+    )
+
+  }
+
+  object MatchingScores {
+
+    /**
+      * A naive match score function, it predicts the success
+      * of a relationship upon how similar their names are:
+      *
+      * In this example imaginary world, a couple with rather different
+      * names will get along better than partners with similar names
+      *
+      */
+    def namingScore(nameA: String, nameB: String): ((String, String), Int) =
+      (nameA -> nameB -> (nameA diff nameB).size)
+
+  }
+
+  import MatchingScores._
+  import CandidateSelectionSystem._
+
+  /**
+    * A couple matching algorithm based on a score function
+    * leveraging Applicative to easily apply the evaluation.
+    */
+  def matchScores: List[((String, String), Int)] =
+    Applicative[List].pure((namingScore(_, _)).curried) ap {
+      femaleCandidates.map(_.toLowerCase)
+    } ap {
+      maleCandidates.map(_.toLowerCase)
+    }
+
+  val bestMatch = matchScores.maxBy(_._2)
+  val worstMatch = matchScores.minBy(_._2)
+
+  println {
+    val (goodNameA, goodNameB) = bestMatch._1
+    val (badNameA, badNameB) = worstMatch._1
+    s"""
+       |$goodNameA and $goodNameB will get along
+       |$badNameA and $badNameB probably not, their names are to similar,
+       |that can only drive to confusion ;-P
+     """.stripMargin
+  }
+
+}
