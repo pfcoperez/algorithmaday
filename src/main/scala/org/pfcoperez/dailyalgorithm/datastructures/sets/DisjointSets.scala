a14cd19b8543cd916ac8488c7925cbc9bb966f68
pfcoperez <contact@pablofranciscoperez.info>
2017-03-20 14:46:56 +0100

    Simplified path compression

--- src/main/scala/org/pfcoperez/dailyalgorithm/datastructures/sets/DisjointSets.scala
+++ src/main/scala/org/pfcoperez/dailyalgorithm/datastructures/sets/DisjointSets.scala
@@ -14,22 +14,23 @@ class DisjointSets[T] private(private val entries: Map[T, Entry[T]] = Map.empty)
     */
   def union(a: T, b: T): (Boolean, DisjointSets[T]) = {
 
-    lazy val result: Option[DisjointSets[T]] = {
+    val result: Option[DisjointSets[T]] = {
       val (opa, phase1) = find(a)
       for {
         pa <- opa
-        (opb, phase2) = phase1.find(a)
+        (opb, phase2) = phase1.find(b)
         pb <- opb
+
       } yield {
         val ((parent, parentEntry: Entry[T]), (child, childEntry: Entry[T])) = {
           import phase2.{entries => flatEntries}
-          val Seq(aEntry, bEntry) = Seq(a,b).map(flatEntries)
-          val parent_child = (a -> aEntry, b -> bEntry)
-          if(aEntry.rank >= bEntry.rank) parent_child else parent_child.swap
+          val Seq(paEntry, pbEntry) = Seq(pa,pb).map(flatEntries)
+          val parent_child = (pa -> paEntry, pb -> pbEntry)
+          if(paEntry.rank >= pbEntry.rank) parent_child else parent_child.swap
         }
         new DisjointSets[T] (
           phase2.entries ++ Seq(
-            child -> childEntry.copy(parent = Some(parent)),
+            child -> childEntry.copy(parent = parent),
             parent -> parentEntry.copy(rank = Math.max(parentEntry.rank, childEntry.rank+1))
           )
         )
@@ -38,27 +39,45 @@ class DisjointSets[T] private(private val entries: Map[T, Entry[T]] = Map.empty)
     result.isDefined -> result.getOrElse(this)
   }
 
+  /**
+    * Checks whether or not a value is present in the disjoint sets collection
+    * @param v label to be found within the data structure
+    * @return Check result
+    */
   def contains(v: T): Boolean = entries contains v
 
+  /**
+    * Find the label of the provided value.
+    * @param v Value whose label is to be found
+    * @return 'None' if the value doesn't exist, Some(label) otherwise
+    */
   def find(v: T): (Option[T], DisjointSets[T]) = {
-    val newSt = flattenBranch(v)
-    newSt.entries(v).parent -> newSt
+    val newState = entries.get(v) map { _ =>
+      flattenBranch(v)
+    }
+    (newState map { st => st.entries(v).parent }, newState.getOrElse(this))
   }
 
+  /**
+    * Add a value to this datastructure
+    * @param v Value to be added
+    * @return New [[DisjointSets]]'s state.
+    */
   def +(v: T): DisjointSets[T] = {
     if(entries contains v) this
-    else new DisjointSets(entries + (v -> Entry(0, None)))
+    else new DisjointSets(entries + (v -> Entry(0, v)))
   }
 
-  private def flattenBranch(label: T): DisjointSets[T] =
+  private def flattenBranch(label: T, toPropagate: List[(T,Entry[T])] = Nil): DisjointSets[T] =
     entries(label) match {
-      case Entry(_, None) => this
-      case Entry(rank, Some(parent)) =>
-        val Entry(_, grandpa) = entries(parent)
-        if(grandpa.isEmpty)
-          new DisjointSets[T](entries + (label -> Entry[T](rank, Some(parent))))
-        else
-          new DisjointSets[T](entries + (label -> Entry(rank, grandpa))) flattenBranch label
+      case Entry(_, parent) if parent == label =>
+        val newEntries = entries ++ {
+          toPropagate map { case (prevV, prevE) =>
+            prevV -> prevE.copy(parent = label)
+          }
+        }
+        new DisjointSets(newEntries)
+      case entry @ Entry(_, parent) => flattenBranch(parent, (label, entry)::toPropagate)
     }
 
 }
@@ -66,8 +85,8 @@ class DisjointSets[T] private(private val entries: Map[T, Entry[T]] = Map.empty)
 object DisjointSets {
 
   def apply[T](labels: T*): DisjointSets[T] = new DisjointSets[T](
-    labels.map(l => l -> Entry(0, Option.empty[T])).toMap
+    labels.map(l => l -> Entry(0, l)) toMap
   )
 
-  private case class Entry[T](rank: Int, parent: Option[T])
+  private case class Entry[T](rank: Int, parent: T)
 }
