07270f5424b1d825822d3e45d357d9a6eafd1239
Pablo Fco. Pérez Hidalgo <pfperez@stratio.com>
2017-03-22 17:54:31 +0100

    Initial monadic state disjoint sets wrapper

---
+++ src/main/scala/org/pfcoperez/dailyalgorithm/datastructures/sets/MonadicDisjointSets.scala
@@ -0,0 +1,16 @@
+package org.pfcoperez.dailyalgorithm.datastructures.sets
+
+import cats._
+import cats.data.State
+
+object MonadicDisjointSets {
+
+  def findState[T](v: T) = State[DisjointSets[T], Option[T]](
+    disjointSets => disjointSets.find(v)
+  )
+
+  def unionState[T](a: T, b: T) = State[DisjointSets[T], Boolean](
+    disjointSets => disjointSets.union(a, b)
+  )
+
+}
