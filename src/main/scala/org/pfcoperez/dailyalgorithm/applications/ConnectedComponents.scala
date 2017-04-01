755d5f6231b06d7e6453a25c48f416c12caf46b3
pfcoperez <contact@pablofranciscoperez.info>
2017-04-01 13:44:40 +0200

    non-tested CCs (not using and using DisjointSets) implementations.

--- src/main/scala/org/pfcoperez/dailyalgorithm/applications/ConnectedComponents.scala
+++ src/main/scala/org/pfcoperez/dailyalgorithm/applications/ConnectedComponents.scala
@@ -3,31 +3,53 @@ package org.pfcoperez.algorithmaday.applications
 
 trait ConnectedComponentsOps {
 
-  def connectedNodes[T](nodes: Set[T], archs: Seq[(T, T)]): Seq[Set[T]] = {
-    def connectedNodes(remainingArchs: Seq[(T,T)], components: Union[T]): Seq[Set[T]] =
-      if(remainingArchs.isEmpty) components.sets
-      else {
-        val (from, to) = remainingArchs.head
-        connectedNodes(remainingArchs.tail, components.join(from, to))
-      }
-  
-    connectedNodes(archs, emptyUnion)
-  }
-
-  protected def emptyUnion[T]: Union[T]
-
-  protected trait Union[T] {
-    def join(a: T, b: T): Union[T]
-    def sets: Seq[Set[T]]
-  }
-
+  def connectedNodes[T](nodes: Set[T], archs: Seq[(T, T)]): Seq[Set[T]]
 
 }
 
 object RegularSetsConnectedComponentsOps extends ConnectedComponentsOps {
 
+  def connectedNodes[T](nodes: Set[T], archs: Seq[(T, T)]): Seq[Set[T]] = {
+    val g = (nodes.toSeq.map(n => n -> List.empty[T]).toMap /: archs) {
+      case (prevG, (from, to)) => prevG + (from -> (to::prevG(from)))
+    }
+
+    def dfs(
+      toVisit: List[T],
+      partition: List[Set[T]],
+      visited: Set[T] = Set.empty
+    ): Seq[Set[T]] =
+      toVisit match {
+        case Nil => partition
+        case current::remaining =>
+          if(visited contains current) dfs(remaining, partition, visited)
+          else {
+            val children = g(current)
+            val updatedPartition: List[Set[T]] = {
+              val currentPart::prevParts = partition
+              if(currentPart contains current) (currentPart ++ children)::prevParts
+              else Set.empty[T]::partition
+            }
+            dfs(children:::remaining, updatedPartition, visited + current)
+          }
+      }
+
+    if(nodes.isEmpty) Seq()
+    else dfs(nodes.head::Nil, Set(nodes.head)::Nil, Set())
+
+  }
+
 }
 
 object DisjointSetsConnectedComponentsOps extends ConnectedComponentsOps {
+
+  import org.pfcoperez.dailyalgorithm.datastructures.sets.DisjointSets
+
+  def connectedNodes[T](nodes: Set[T], archs: Seq[(T, T)]): Seq[Set[T]] = {
+    val dsets = (DisjointSets(nodes.toSeq:_*) /: archs) {
+      case (dsets, (from, to)) => dsets.union(from, to)._1
+    }
+    dsets.toSets._2.values.toSeq
+  }
 
 }
