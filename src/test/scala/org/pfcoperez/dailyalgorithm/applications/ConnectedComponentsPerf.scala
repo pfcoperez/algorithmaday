8d41d71509ae1f4119f8e47afaa3f5e69d7bdc68
pfcoperez <contact@pablofranciscoperez.info>
2017-04-02 16:05:52 +0200

    CCs: Performance with text reporter

--- src/test/scala/org/pfcoperez/dailyalgorithm/applications/ConnectedComponentsPerf.scala
+++ src/test/scala/org/pfcoperez/dailyalgorithm/applications/ConnectedComponentsPerf.scala
@@ -1,26 +1,29 @@
 package org.pfcoperez.algorithmaday.applications
 
 import org.scalameter.api._
+import org.scalameter.picklers.noPickler._
 
 object ConnectedComponentsBenchmark extends Bench.LocalTime {
+  
+  type Graph = (Set[Int], Seq[(Int, Int)])
 
-  def generateForest(nNodes: Int, nArchs: Int): (Set[Int], Seq[(Int, Int)]) = {
+  def generateForest(nNodes: Int, nArchs: Int): Graph = {
     import org.scalacheck.Gen
     val nodes = Seq(1 to nNodes: _*)
     val archGenerator: Gen[(Int, Int)] =
       for {
         from <- Gen.oneOf(nodes)
-        to <- Gen.oneOf(nodes.filterNot(_ == from))
+        to <- Gen.oneOf(nodes/*.filterNot(_ == from)*/)
       } yield (from, to)
     nodes.toSet -> Gen.listOfN(nArchs, archGenerator).sample.get
   }
 
-  val graphGenerator = for {
-    nNodes <- Gen.range("No.Nodes")(1, 10000, 250)
-    archsRatio <- Gen.range("Archs Ratio")(0, 240, 20)
+  val graphGenerator: Gen[Graph] = for {
+    nNodes <- Gen.range("No.Nodes")(1, 1000, 250) //(1, 10000, 250)
+    archsRatio <- Gen.range("Archs Ratio")(0, 300, 100) //(0, 240, 20)
   } yield generateForest(nNodes, (nNodes*archsRatio/100.0).toInt)
 
-  val caseGenerator = for {
+  val caseGenerator: Gen[(ConnectedComponentsOps, Graph)] = for {
     graph <- graphGenerator
     implementation <- Gen.enumeration("Implementation")(
       RegularSetsConnectedComponentsOps,
@@ -30,8 +33,9 @@ object ConnectedComponentsBenchmark extends Bench.LocalTime {
 
   performance of "Connected Components" in {
     measure method "connectedNodes" in {
-      using(caseGenerator) curve("Implementation") in { case (implementation, (nodes, archs)) =>
-        implementation.connectedNodes(nodes, archs)
+      using(caseGenerator) curve("Implementation") in {
+        case (implementation: ConnectedComponentsOps, (nodes, archs)) =>
+          implementation.connectedNodes(nodes, archs)
       }
     }
   }
