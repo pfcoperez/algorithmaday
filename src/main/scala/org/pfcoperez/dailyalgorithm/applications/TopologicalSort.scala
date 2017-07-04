e8cf76f7492d12d554ecbe9d7b33ee80e82ba6e1
Pablo Fco. Pérez Hidalgo <contact@pablofranciscoperez.info>
2017-07-04 17:26:00 +0200

    Kahn's topological sort algorith: O(M), M=number of edges

---
+++ src/main/scala/org/pfcoperez/dailyalgorithm/applications/TopologicalSort.scala
@@ -0,0 +1,50 @@
+package org.pfcoperez.dailyalgorithm.applications
+
+object TopologicalSort {
+  
+  type DependencyMatrix[T] = Map[T, Set[T]]
+
+  /**
+    * TopologicalSort: Kahn's algorithm
+    * O(n), n = number of archs in the input DAG
+    * 
+    * @param dm DAG as a dependency matrix.
+    * 
+    */
+  def topologicalSort[T](dm: DependencyMatrix[T]): Option[List[T]] = {
+
+    val destinations0 = dm.values.flatten.groupBy(identity).mapValues(_.size)
+    val s0 = dm.keySet.filterNot(destinations0 contains _)
+
+    def topologicalSort(
+      s: Set[T],
+      destinations: Map[T, Int],
+      dm: DependencyMatrix[T]
+    )(acc: List[T]): Option[List[T]] = {
+      val (sh, remaining) = s.splitAt(1)
+      if(sh.isEmpty) Some(acc) filter { _ =>
+        destinations.values.forall(_ == 0)
+      }
+      else {
+        val from = sh.head
+        val targets = dm.getOrElse(from, Set.empty)
+        val (newS, newDestinations, newDm) =
+          ((remaining, destinations, dm) /: targets) {
+            case ((s, destinations, dm), to) =>
+              val archs2To = destinations(to)
+              (
+                if(archs2To == 1) s + to else s,
+                destinations + (to -> (archs2To-1)),
+                dm + (from -> (targets - to))
+              )
+          }
+        topologicalSort(newS, newDestinations, newDm)(from :: acc)
+      }
+    }
+
+    topologicalSort(s0, destinations0, dm)(Nil)
+  }
+
+}
+
+
---
+++ src/test/scala/org/pfcoperez/dailyalgorithm/applications/TopologicalSortSpec.scala
@@ -0,0 +1,80 @@
+package org.pfcoperez.dailyalgorithm.applications
+
+import org.scalatest.{FlatSpec, Matchers, Inside}
+
+
+class TopologicalSortSpec extends FlatSpec with Matchers with Inside {
+  import TopologicalSort._
+
+  "TopologicalSort algorithm implementation" should "order elements by their dependency tree" in {
+
+    val dm: DependencyMatrix[Char] = Map(
+      'A' -> Set('E'),
+      'B' -> Set('F'),
+      'C' -> Set('G'),
+      'D' -> Set('G'),
+      'E' -> Set('H'),
+      'F' -> Set('H', 'I'),
+      'G' -> Set('I'),
+      'H' -> Set('J'),
+      'I' -> Set('H'),
+      'J' -> Set()
+    )
+
+    inside(topologicalSort(dm)) {
+      case Some(order) =>
+        val node2index = order.zipWithIndex.toMap
+
+        node2index('J') shouldBe 0
+        node2index('H') shouldBe 1
+        node2index('E') should be < node2index('A')
+        node2index('F') should be < node2index('B')
+        node2index('I') should be < node2index('G')
+        node2index('G') should be < node2index('C')
+        node2index('G') should be < node2index('D')
+
+    }
+
+  }
+
+  it should "fail to create an order when there are cyclic dependencies" in {
+
+    val dm: DependencyMatrix[Char] = Map(
+      'A' -> Set('B'),
+      'B' -> Set('C'),
+      'C' -> Set('A')
+    )
+
+    topologicalSort(dm) shouldBe empty
+
+  }
+
+  it should "serve to make a plan to develop interstellar travel" in {
+
+    val technologicalDependencies: DependencyMatrix[String] = Map(
+      "Interstellar Travel" -> Set("Safe cabins", "Warp drive"),
+      "Quantum computing" -> Set(),
+      "Magnetic shields" -> Set("Quantum computing"),
+      "Dark matter confinement" -> Set("Quantum computing"),
+      "Safe cabins" -> Set("Magnetic shields"),
+      "Warp drive" -> Set("Dark matter confinement")
+    )
+
+    inside(topologicalSort(technologicalDependencies)) {
+      case Some(plan) =>
+
+        plan.head shouldBe "Quantum computing"
+        plan.last shouldBe "Interstellar Travel"
+
+        val planIndexes: Map[String, Int] = plan.zipWithIndex.toMap
+
+        planIndexes("Dark matter confinement") should be < planIndexes("Warp drive")
+        planIndexes("Magnetic shields") should be < planIndexes("Safe cabins")
+
+
+    }
+
+  }
+
+
+}
