556890994f21f5a487704d746681bf6684ef8cf0
Pablo Fco. PÃ©rez Hidalgo <contact@pablofranciscoperez.info>
2017-08-21 14:58:44 +0200

    Karger Minimum Cut to partition undirected graphs. O(n^2m)

--- src/main/scala/org/pfcoperez/dailyalgorithm/applications/MinCutKarger.scala
+++ src/main/scala/org/pfcoperez/dailyalgorithm/applications/MinCutKarger.scala
@@ -6,42 +6,82 @@ object MinCutKarger {
   import org.pfcoperez.dailyalgorithm.numericmethods.random.RandomGenerator
   import org.pfcoperez.dailyalgorithm.datastructures.graphs.undirected._
 
+  /*
+   * Randomized minimum cut.
+   * Given an undirected connected node, finds a sequence of nodes to 
+   * be removed in order to make a partition.
+   * O(n^2 m) [Worst case], n = number of nodes, m = number of edges
+   * 
+   */
   def minCut[Node: Ordering, W](
     ug: UndirectedWeighedGraph[Node, W],
     randomSeed: Long): Option[Seq[Edge[Node, W]]] = {
 
     type EdgeType = Edge[Node, W]
 
+    val labels = ug.edges.toSet.zipWithIndex map {
+      case (node, label) => node -> Set(label)
+    } toMap
+
+    /**
+      * Merge two nodes keeping track of the original
+      * labels.
+      * 
+      * O(nm) [Worst case], n = number of nodes, m = number of edges
+      * 
+      */
     def merge(
-      ug: UndirectedWeighedGraph[Node, W])(assimilated: Node, host: Node): UndirectedWeighedGraph[Node, W] =
-      ((ug - assimilated) /: ug.edges(assimilated)) {
-        case (updatedGraph, edge @ Edge(from, to, w)) =>
-          Seq(from, to).find(!Set(assimilated, host).contains(_)) map { outNode =>
-            updatedGraph + Edge(host, outNode, w)
-          } getOrElse updatedGraph
-
-      }
-
-    @scala.annotation.tailrec
-    def minCut(
       ug: UndirectedWeighedGraph[Node, W],
-      randomGenerator: RandomGenerator[Int]): Option[Seq[Edge[Node, W]]] = {
+      edge2label: Map[EdgeType, Set[Int]])(
+      assimilated: Node, host: Node): (UndirectedWeighedGraph[Node, W], Map[EdgeType, Set[Int]]) = {
 
-      ug.nodes.size match {
-        case 0 | 1 => None
-        case 2 =>
-          val Seq(a, b) = ug.nodes.toSeq
-          Some(ug.edges)
-        case _ =>
-          val edges = ug.edges.toVector
-          val (newGenerator, randomInteger) = randomGenerator.next
-          val edge @ Edge(a, b, _) = edges(Math.abs(randomInteger) % edges.size)
+      // The assimilated node gets deleted, its edges assimilated as ...
+      (((ug - assimilated), edge2label) /: ug.edges(assimilated)) {
+        case ((updatedGraph, labels), edge @ Edge(from, to, w)) =>
+          Seq(from, to).find(!Set(assimilated, host).contains(_)) map { outNode =>
+            val updatedEdge = Edge(host, outNode, w) // ...new ones
 
-          minCut(merge(ug)(a, b), newGenerator)
+            val updatedLabels = (labels - edge) + { // Track original graph labels
+              (updatedEdge, labels.getOrElse(updatedEdge, Set.empty) ++ labels.getOrElse(edge, Set.empty))
+            }
+
+            // The new edge is added to the updated graph, without the original edge.
+            ((updatedGraph + updatedEdge), updatedLabels)
+
+          } getOrElse ((updatedGraph, labels))
+
       }
     }
 
-    minCut(ug, new IntRandom(randomSeed))
+    def minCut(
+      state: (UndirectedWeighedGraph[Node, W], Map[EdgeType, Set[Int]]),
+      randomGenerator: RandomGenerator[Int]): Option[Seq[Edge[Node, W]]] = {
+
+      val (currentUg, currentLabels) = state
+
+      // How many nodes does the current graph have?
+      currentUg.nodes.size match {
+        case 0 | 1 => None // It is possible to find a partition of one node graph.
+        case 2 => // If only two node remain, a partition is created when disconnecting one from the other.
+          val labelsSet = currentUg.edges.flatMap(currentLabels).toSet
+          Some {
+            // These two nodes are, indeed, classes of nodes of the original graph, the edges
+            // connecting them are the edges that should be removed to create the partition.
+            ug.edges.filter(edge => labelsSet.contains(labels(edge).head))
+          } filter (_.nonEmpty)
+        case _ => // There are more than 2 nodes, the graph hasn't been simplified enough so...
+          val edges = currentUg.edges.toVector
+          val (newGenerator, randomInteger) = randomGenerator.next
+          // Choose a random edge
+          val edge @ Edge(a, b, _) = edges(Math.abs(randomInteger) % edges.size)
+          // And run the algorithm with the graph resulting from merging
+          // the nodes connected by the randomly chosen edge.
+          minCut(merge(currentUg, currentLabels)(a, b), newGenerator)
+      }
+
+    }
+
+    minCut((ug, labels), new IntRandom(randomSeed))
 
   }
 
--- src/test/scala/org/pfcoperez/dailyalgorithm/applications/MinCutKargerSpec.scala
+++ src/test/scala/org/pfcoperez/dailyalgorithm/applications/MinCutKargerSpec.scala
@@ -3,33 +3,35 @@ package org.pfcoperez.dailyalgorithm.applications
 import MinCutKarger._
 import org.pfcoperez.dailyalgorithm.datastructures.graphs.undirected._
 
-import org.scalatest.{ FlatSpec, Matchers }
+import org.scalatest.{ WordSpec, Matchers }
 
-class MinCutKargerSpec extends FlatSpec with Matchers {
-
-  val graphA = {
-    val nodes = (1 to 6).toSet
-
-    val edges: Seq[Edge[Int, NoWeight]] = Seq(
-      Edge(1, 2), Edge(2, 3), Edge(3, 1),
-      Edge(3, 4),
-      Edge(4, 5), Edge(5, 6), Edge(6, 4))
-
-    val ug = (UndirectedGraph(nodes, Nil) /: edges) { (ug, edge) => ug + edge }
-
-    println(ug.edges.toList)
-
-    ug
-
-  }
+class MinCutKargerSpec extends WordSpec with Matchers {
 
   def timeBasedSeed(): Long = System.currentTimeMillis()
 
-  "MinCutKarger implementation" should "be able to find a cut" in {
+  "MinCutKarger implementation" when {
 
-    val res = minCut(graphA, timeBasedSeed())
+    val graphA = {
+      val nodes = (1 to 6).toSet
 
-    println(res.map(_.toList))
+      val edges: Seq[Edge[Int, NoWeight]] = Seq(
+        Edge(1, 2), Edge(2, 3), Edge(3, 1),
+        Edge(3, 4),
+        Edge(4, 5), Edge(5, 6), Edge(6, 4))
+
+      (UndirectedGraph(nodes, Nil) /: edges) { (ug, edge) => ug + edge }
+    }
+
+    "used to try to find the minimum cut for a graph" should {
+
+      (0 until 20) foreach { it =>
+        s"provide a feasible cut at iteration $it" in {
+          val res = minCut(graphA, timeBasedSeed())
+          res.get.size should be <= 2
+        }
+      }
+
+    }
 
   }
 
