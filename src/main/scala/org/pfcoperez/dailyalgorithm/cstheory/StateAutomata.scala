b2d0477abbe1ef09da364930bef9ce79823e2a41
Pablo Fco. Pérez Hidalgo <contact@pablofranciscoperez.info>
2017-11-28 12:17:59 +0100

    Finite Automaton minimization.

--- src/main/scala/org/pfcoperez/dailyalgorithm/cstheory/StateAutomata.scala
+++ src/main/scala/org/pfcoperez/dailyalgorithm/cstheory/StateAutomata.scala
@@ -6,28 +6,31 @@ import scala.util.{ Either, Right, Left }
 
 import org.pfcoperez.dailyalgorithm.datastructures.graphs.directed
 
+import scala.language.postfixOps
+
 object StateAutomata {
 
   trait Error
   case class NoTransition[State, Entry](from: State, entry: Entry) extends Error
 
-  case class AutomatonStep[State, Output](nextState: State,
-                                          toFinalSt: Boolean,
-                                          output: Option[Output])
+  case class AutomatonStep[State, Output](
+    nextState: State,
+    toFinalSt: Boolean,
+    output: Option[Output])
 
   class Automaton[State, Entry, Output] private[StateAutomata] (
     val initialState: State,
     val finalStates: Set[State],
-    private val transitionMatrix: Map[State, Map[Entry, (State, Option[Output])]]) {
+    val transitionMatrix: Map[State, Map[Entry, (State, Option[Output])]]) {
 
     /**
-      * Creates a new stream of transitions: To which state? is it final? what is the transition output?
-      * O(1)
-      * The generated stream computes each new element in O(1), n transitions are computed in O(n)
-      */
-    def materializeComputation(input: Stream[Entry],
-                               from: State = initialState
-                              ): Stream[Either[Error, AutomatonStep[State, Output]]] =
+     * Creates a new stream of transitions: To which state? is it final? what is the transition output?
+     * O(1)
+     * The generated stream computes each new element in O(1), n transitions are computed in O(n)
+     */
+    def materializeComputation(
+      input: Stream[Entry],
+      from: State = initialState): Stream[Either[Error, AutomatonStep[State, Output]]] =
       input.scanLeft(AutomatonStep(from, finalStates contains from, Option.empty[Output]).asRight[Error]) {
         case (Right(AutomatonStep(prevSt, _, _)), input) =>
           {
@@ -39,7 +42,102 @@ object StateAutomata {
         case (error, _) => error
       }
 
-    val states: Set[State] = transitionMatrix.keySet + initialState
+    /**
+     * Merge two automaton states into a single one
+     * O(n + m), n = number of states, m = number of transitions
+     */
+    def mergeStates(a: State, b: State): Automaton[State, Entry, Output] =
+      if(a == b) this
+      else {
+        val redirectedTransitions = transitionMatrix mapValues {
+          _.mapValues {
+            case transition @ (to, output) =>
+              if (to == b) a -> output
+              else transition
+          }
+        }
+        val bTransitions = redirectedTransitions.getOrElse(b, Map.empty)
+        val newTransitionMatrix =
+          redirectedTransitions + ((a, redirectedTransitions.getOrElse(a, Map.empty) ++ bTransitions))
+        val newInitialState = if (initialState == b) a else initialState
+        val newFinalStates = finalStates ++ Set(a).filter(_ => finalStates contains b)
+        new Automaton(newInitialState, newFinalStates - b, newTransitionMatrix - b)
+      }
+
+    lazy val states: Set[State] = transitionMatrix.keySet + initialState
+
+    lazy val unreachableStates: Set[State] = {
+      val destinations = {
+        for {
+          rows <- transitionMatrix.values
+          (to, _) <- rows.values
+        } yield to
+      }.toSet
+      states -- destinations
+    }
+
+    def removeUnreachable: Automaton[State, Entry, Output] = {
+      new Automaton(initialState, finalStates -- unreachableStates, transitionMatrix -- unreachableStates)
+    }
+
+  }
+
+  object Automaton {
+
+    /**
+      * Produce a minimized version, through Myhill-Nerode theorem, of the input automaton.
+      * O(n^2 m^2), n = number of states, m = number of transitions.
+      */
+    def minimize[State, Entry, Output](automaton: Automaton[State, Entry, Output]): Automaton[State, Entry, Output] = {
+      val indexedStates = automaton.states.toVector
+      def isFinal(st: State): Boolean = automaton.finalStates contains st
+
+      /* The equivalence classes of states are:
+      - States leading to final states after k transitions
+      - States leading to non-final states after k transitions */
+
+      val classZero = { // Equivalence classes after 0 transitions
+        for {
+          i <- 0 until indexedStates.size
+          j <- 0 until i
+          a = indexedStates(i)
+          b = indexedStates(j)
+          if (!(isFinal(a) ^ isFinal(b)))
+        } yield (a, b)
+      } toSet
+
+      /* Equivalence classes after n transitions, up to when no additional
+      unions can be performed */
+      def classN(prevNonFinalClass: Set[(State, State)]): Set[(State, State)] = {
+        val toFinalClass = prevNonFinalClass find {
+          case candidate @ (a, b) =>
+            val inputsToTest = for {
+              st <- Set(a, b)
+              inputs <- automaton.transitionMatrix(st).keySet
+            } yield inputs
+            inputsToTest exists { input =>
+              val Seq(stepA, stepB) = Seq(a, b) map { st =>
+                for {
+                  stTransitions <- automaton.transitionMatrix.get(st)
+                  (to, _) <- stTransitions.get(input)
+                } yield to
+              }
+              (stepA zip stepB) exists { candidateTransition =>
+                !prevNonFinalClass.contains(candidateTransition)
+              }
+            }
+        }
+        if (toFinalClass.nonEmpty) classN(prevNonFinalClass - toFinalClass.get)
+        else prevNonFinalClass
+      }
+      val (newAutomaton, _) = ((automaton, Map.empty[State, State]) /: classN(classZero)) {
+        case ((prevAutomaton, equivalences), (pa, pb)) =>
+          val Seq(a, b) = Seq(pa, pb).map(s => equivalences.getOrElse(s, s))
+          (prevAutomaton.mergeStates(a, b), equivalences + (b -> a))
+      }
+      newAutomaton.removeUnreachable
+    }
+
   }
 
   object AutomatonBuilder {
@@ -50,8 +148,7 @@ object StateAutomata {
     case class TransitionsBuilder[State, Entry, Output](
       initial: State,
       finalStates: Set[State],
-      transitionMatrix: Map[State, Map[Entry, (State, Option[Output])]]
-    ) {
+      transitionMatrix: Map[State, Map[Entry, (State, Option[Output])]]) {
 
       def in(state: State) = FromSt(state)
       def inFinal(state: State) = FromSt(state, true)
@@ -72,12 +169,11 @@ object StateAutomata {
         def goTo(newState: State): TransitionsBuilder[State, Entry, Output] = {
           val newTransitionMatrix = {
             val updatedRow = transitionMatrix.getOrElse(
-              from.in, Map.empty[Entry, (State, Option[Output])]
-            ) + (when -> (newState, output))
+              from.in, Map.empty[Entry, (State, Option[Output])]) + (when -> (newState, output))
             transitionMatrix + (from.in -> updatedRow)
           }
-          val newFinalStates = if(from.isFinal) finalStates + from.in else finalStates
-          copy(finalStates = newFinalStates,transitionMatrix = newTransitionMatrix)
+          val newFinalStates = if (from.isFinal) finalStates + from.in else finalStates
+          copy(finalStates = newFinalStates, transitionMatrix = newTransitionMatrix)
         }
 
       }
--- src/test/scala/org/pfcoperez/dailyalgorithm/cstheory/StateAutomataSpec.scala
+++ src/test/scala/org/pfcoperez/dailyalgorithm/cstheory/StateAutomataSpec.scala
@@ -1,11 +1,16 @@
 package org.pfcoperez.dailyalgorithm.cstheory
 
-import org.pfcoperez.dailyalgorithm.cstheory.StateAutomata.{AutomatonBuilder, AutomatonStep, NoTransition}
-import org.scalatest.{Inside, Inspectors, Matchers, WordSpec}
+import org.pfcoperez.dailyalgorithm.cstheory.StateAutomata.{ AutomatonBuilder, AutomatonStep, NoTransition, Automaton }
+import org.scalatest.{ Inside, Inspectors, Matchers, WordSpec }
 
 class StateAutomataSpec extends WordSpec with Matchers with Inspectors with Inside {
 
+  import StateAutomata.Error
+
   "StateAutomata" when {
+
+    def justOutput[State, Output](res: Stream[Either[Error, AutomatonStep[State, Output]]]
+                                 ): Stream[Either[Error, Option[Output]]] = res.map(_.right.map(_.output))
 
     "modeling finite state machines" should {
 
@@ -31,6 +36,7 @@ class StateAutomataSpec extends WordSpec with Matchers with Inspectors with Insi
 
         results count {
           case Right(AutomatonStep(_, isFinal, _)) => isFinal
+          case _ => false
         } shouldBe 2
 
         forAll(results) {
@@ -46,6 +52,30 @@ class StateAutomataSpec extends WordSpec with Matchers with Inspectors with Insi
             fromSt shouldBe 3
             input shouldBe 'a'
         }
+      }
+
+      "be easily minimized using Myhill-Nerode theorem" in {
+
+        val wordRecognizerAutomaton = startingWith[Int, Char, String](0)
+          .in(0).when('g').goTo(1)
+          .in(1).when('n').yielding("Almost there!").goTo(2)
+          .in(2).when('u').yielding("May the lambda be with you!").goTo(3)
+          .inFinal(3).when('!').goTo(4)
+          .in(4).when('g').goTo(5)
+          .in(5).when('n').yielding("Almost there!").goTo(6)
+          .in(6).when('u').yielding("May the lambda be with you!").goTo(7)
+          .inFinal(7).when('!').goTo(0)
+          .done
+
+        val minimizedAutomaton = Automaton.minimize(wordRecognizerAutomaton)
+
+        lazy val infiniteGnus: Stream[Char] = "gnu!".toStream #::: infiniteGnus
+
+        val expectedResult = justOutput(wordRecognizerAutomaton.materializeComputation(infiniteGnus)).take(21)
+
+        justOutput(minimizedAutomaton.materializeComputation(infiniteGnus)).take(21) shouldBe expectedResult
+        minimizedAutomaton.states.size should be < wordRecognizerAutomaton.states.size
+
       }
 
     }
